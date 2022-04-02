3e088c23a80454077b95d50a21c26351fc32ba16
amitkumar <amit.suhag007@gmail.com>
2022-04-02 16:14:08 +0530

    setting default profile for greeting service

--- src/main/java/com/example/spring5webapp/services/I18NSpanishService.java
+++ src/main/java/com/example/spring5webapp/services/I18NSpanishService.java
@@ -3,7 +3,7 @@ package com.example.spring5webapp.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18NSpanishService implements GreetingService {
     @Override
